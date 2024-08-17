31776584bdea06c4049d8ec44330e9007b0be988
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-08-17 16:51:16 +0200

    The database class - part 2

--- app/controller/Page.php
+++ app/controller/Page.php
@@ -1,18 +1,27 @@
 <?php
 
-class Page extends Controller {
+class Page extends Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
 
         $this->postModel = $this->model('Post');
+
     }
 
-    public function index() {
-        $data = ['title' => 'Welcome'];
+    public function index()
+    {
+        $posts = $this->postModel->getPosts();
+        $data = [
+            'title' => 'Welcome',
+            'posts' => $posts
+        ];
         $this->view('pages/index', $data);
     }
 
-    public function about() {
+    public function about()
+    {
         $data = ['title' => 'About Us'];
         $this->view('pages/about', $data);
     }
