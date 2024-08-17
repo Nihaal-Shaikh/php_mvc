09f3c93a47eabdf532726039ae19498c98c7df91
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-08-17 17:04:51 +0200

    Clean up

--- app/controller/Page.php
+++ app/controller/Page.php
@@ -6,16 +6,12 @@ class Page extends Controller
     public function __construct()
     {
 
-        $this->postModel = $this->model('Post');
-
     }
 
     public function index()
     {
-        $posts = $this->postModel->getPosts();
         $data = [
-            'title' => 'Welcome',
-            'posts' => $posts
+            'title' => 'PHP MVC',
         ];
         $this->view('pages/index', $data);
     }
--- app/view/pages/index.php
+++ app/view/pages/index.php
@@ -1,8 +1,4 @@
 <?php require APPROOT . '/view/inc/header.php'; ?>
 <h1><?php echo $data['title'] ?></h1>
-<ul>
-    <?php foreach ($data['posts'] as $post): ?>
-        <li><?php echo $post->title; ?></li>
-    <?php endforeach; ?>
-</ul>
+<p>This is the PHP MVC framework. Please refer to the docs on how to use it</p>
 <?php require APPROOT . '/view/inc/footer.php'; ?>
